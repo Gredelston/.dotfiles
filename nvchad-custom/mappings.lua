ddfaac414b7536111cb2d0ccd8714e033e03dae6
Greg Edelston <gredelston@google.com>
2023-02-07 15:05:58 -0700

    Add fzf shortcut

--- nvchad-custom/mappings.lua
+++ nvchad-custom/mappings.lua
@@ -5,6 +5,7 @@ M.general = {
   n = {
     [";"] = {":", "enter command mode", opts = { nowait = true } },
     [":"] = {";", "Repeat latest f, t, F, or T [count] times. See cpo-;"},
+    ["<C-f>"] = {":Telescope fd<CR>", "Open file fuzzy-finder"},
   },
   i = {
     ["jk"] = {"<ESC>", "Exit insert mode."},
