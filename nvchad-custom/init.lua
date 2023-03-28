71f09bdebeba3161d3e4b2a53b176ceee87af55d
Greg Edelston <gredelston@google.com>
2023-03-28 11:14:23 -0600

    Highlight current context in rainbow

--- nvchad-custom/init.lua
+++ nvchad-custom/init.lua
@@ -5,4 +5,12 @@ vim.api.nvim_create_autocmd(
   }
 )
 
+-- Indentation highlighting colors
+-- https://github.com/lukas-reineke/indent-blankline.nvim#with-custom-gindent_blankline_char_highlight_list
 vim.opt.termguicolors = true
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
--- nvchad-custom/plugins.lua
+++ nvchad-custom/plugins.lua
@@ -6,6 +6,15 @@ local plugins = {
       require("indent_blankline").setup {
         space_char_blankline = " ",
         show_current_context = true,
+        show_current_context_start = true,
+        context_highlight_list = {
+          "IndentBlanklineIndent1",
+          "IndentBlanklineIndent2",
+          "IndentBlanklineIndent3",
+          "IndentBlanklineIndent4",
+          "IndentBlanklineIndent5",
+          "IndentBlanklineIndent6",
+        }
       }
     end
   },
