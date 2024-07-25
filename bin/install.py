eab33319a6efc1d62574276a8ae8b24ac7d576ce
Greg Edelston <gredelston@gmail.com>
2024-07-25 15:27:12 -0600

    Format install.py with black

--- bin/install.py
+++ bin/install.py
@@ -10,25 +10,25 @@ import sys
 from typing import Any
 
 
-BASHRC = '.bashrc'
-GITCONFIG = '.gitconfig'
-INITVIM = 'init.vim'
-TMUX_CONF = '.tmux.conf'
-VIMRC = '.vimrc'
-ZSHRC = '.zshrc'
+BASHRC = ".bashrc"
+GITCONFIG = ".gitconfig"
+INITVIM = "init.vim"
+TMUX_CONF = ".tmux.conf"
+VIMRC = ".vimrc"
+ZSHRC = ".zshrc"
 
 HOME = Path.home()
 HOME_BASHRC = HOME / BASHRC
-HOME_INITVIM = HOME / '.config' / 'nvim' / INITVIM
+HOME_INITVIM = HOME / ".config" / "nvim" / INITVIM
 HOME_GITCONFIG = HOME / GITCONFIG
 HOME_TMUX_CONF = HOME / TMUX_CONF
 HOME_VIMRC = HOME / VIMRC
 HOME_ZSHRC = HOME / ZSHRC
 
-DF = HOME / '.dotfiles'
+DF = HOME / ".dotfiles"
 DF_BASHRC = DF / BASHRC
-DF_CROS_SDK_BASHRC = DF / '.cros_sdk_bashrc'
-DF_CROS_SDK_ZSHRC = DF / '.cros_sdk_zshrc'
+DF_CROS_SDK_BASHRC = DF / ".cros_sdk_bashrc"
+DF_CROS_SDK_ZSHRC = DF / ".cros_sdk_zshrc"
 DF_INITVIM = DF / INITVIM
 DF_GITCONFIG = DF / GITCONFIG
 DF_TMUX_CONF = DF / TMUX_CONF
@@ -39,19 +39,20 @@ DF_ZSHRC = DF / ZSHRC
 def parse_args(argv: list[str]) -> argparse.Namespace:
     """Interpret command-line options for this script."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--pretend",
-                        help="Don't actually change anything",
-                        action="store_true")
+    parser.add_argument(
+        "--pretend", help="Don't actually change anything", action="store_true"
+    )
     return parser.parse_args(argv)
 
 
 def _in_chroot() -> bool:
     """Determine whether this script is being run inside the ChromeOS chroot."""
-    return os.path.isfile('/etc/cros_chroot_version')
+    return os.path.isfile("/etc/cros_chroot_version")
 
 
 class FSInterface:
     """Class to interact with the filesystem."""
+
     def __init__(self, pretend: bool = False) -> None:
         self.pretend = pretend
 
@@ -59,30 +60,30 @@ class FSInterface:
         """Add lines to the end of a file."""
         assert isinstance(lines, list)
         if not lines:
-            logging.info('No lines to append to %s', filepath)
+            logging.info("No lines to append to %s", filepath)
             return
         if self.pretend:
-            logging.info('PRETEND: Append these lines to %s:', filepath)
-            lines = [line.replace('\t', ' '*4) for line in lines]
+            logging.info("PRETEND: Append these lines to %s:", filepath)
+            lines = [line.replace("\t", " " * 4) for line in lines]
             longest = max([len(line) for line in lines] or [0])
-            logging.info('-' * (longest + 4))
+            logging.info("-" * (longest + 4))
             for line in lines:
-                logging.info('| %s |', line.ljust(longest))
-            logging.info('-' * (longest + 4))
+                logging.info("| %s |", line.ljust(longest))
+            logging.info("-" * (longest + 4))
             return
-        with filepath.open('a') as f:
-            logging.info('Appending %d lines to %s', len(lines), filepath)
+        with filepath.open("a") as f:
+            logging.info("Appending %d lines to %s", len(lines), filepath)
             for line in lines:
-                f.write('\n')
+                f.write("\n")
                 f.write(line)
 
     def create_link(self, target: Path, link_path: Path) -> None:
         """Create a link to target at link_path."""
         assert target.is_file()
         if self.pretend:
-            logging.info('PRETEND: Create link to %s at %s', target, link_path)
+            logging.info("PRETEND: Create link to %s at %s", target, link_path)
             return
-        logging.info('Creating link to %s at %s', target, link_path)
+        logging.info("Creating link to %s at %s", target, link_path)
         link_path.hardlink_to(target)
 
     def ensure_dir_exists(self, dir_path: Path) -> None:
@@ -93,62 +94,61 @@ class FSInterface:
     def mkdir(self, dir_path: Path, **kwargs: Any) -> None:
         """Create a directory."""
         if self.pretend:
-            logging.info('PRETEND: Create directory: {dir_path}')
+            logging.info("PRETEND: Create directory: {dir_path}")
             return
         dir_path.mkdir(**kwargs)
 
 
 def _file_contains_string(filepath: Path, string: str) -> bool:
     with filepath.open() as f:
-        return string in '\n'.join(f.readlines())
+        return string in "\n".join(f.readlines())
 
 
 def setup_bashrc(fsi: FSInterface) -> None:
     """Setup .bashrc, the config file for Bash."""
-    logging.info('Sourcing %s in %s', DF_BASHRC, HOME_BASHRC)
+    logging.info("Sourcing %s in %s", DF_BASHRC, HOME_BASHRC)
     bashrcs_to_source: list[Path] = [DF_BASHRC]
     if _in_chroot():
         bashrcs_to_source.append(DF_CROS_SDK_BASHRC)
-        logging.info('Sourcing the following .bashrc files in %s:', HOME_BASHRC)
+        logging.info("Sourcing the following .bashrc files in %s:", HOME_BASHRC)
         logging.info(bashrcs_to_source)
     lines = []
     for bashrc_to_source in bashrcs_to_source:
-        if _file_contains_string(HOME_BASHRC, f'source {DF_BASHRC}'):
-            logging.info('%s already sourced. Skipping.', bashrc_to_source)
+        if _file_contains_string(HOME_BASHRC, f"source {DF_BASHRC}"):
+            logging.info("%s already sourced. Skipping.", bashrc_to_source)
             continue
-        lines.extend(
-            ['', '# Import my standard .bashrc', f'source {DF_BASHRC}'])
+        lines.extend(["", "# Import my standard .bashrc", f"source {DF_BASHRC}"])
     fsi.append_to_file(HOME_BASHRC, lines)
 
 
 def setup_zshrc(fsi: FSInterface) -> None:
     """Setup .zshrc, the config file for Zsh."""
-    logging.info('Sourcing %s in %s', DF_ZSHRC, HOME_ZSHRC)
+    logging.info("Sourcing %s in %s", DF_ZSHRC, HOME_ZSHRC)
     zshrcs_to_source: list[Path] = [DF_ZSHRC]
     if _in_chroot():
         zshrcs_to_source.append(DF_CROS_SDK_ZSHRC)
-        logging.info('Sourcing the following .zshrc files in %s:', HOME_ZSHRC)
+        logging.info("Sourcing the following .zshrc files in %s:", HOME_ZSHRC)
         logging.info(zshrcs_to_source)
     lines = []
     for zshrc_to_source in zshrcs_to_source:
-        if _file_contains_string(HOME_ZSHRC, f'source {DF_ZSHRC}'):
-            logging.info('%s already sourced. Skipping.', zshrc_to_source)
+        if _file_contains_string(HOME_ZSHRC, f"source {DF_ZSHRC}"):
+            logging.info("%s already sourced. Skipping.", zshrc_to_source)
             continue
-        lines.extend(['', '# Import my standard .zshrc', f'source {DF_ZSHRC}'])
+        lines.extend(["", "# Import my standard .zshrc", f"source {DF_ZSHRC}"])
     fsi.append_to_file(HOME_ZSHRC, lines)
 
 
 def setup_gitconfig(fsi: FSInterface) -> None:
     """Setup .gitconfig, the config file for Git."""
-    logging.info('Including %s in %s', DF_GITCONFIG, HOME_GITCONFIG)
-    lines = ['[include]', f'\tpath = {DF_GITCONFIG}']
+    logging.info("Including %s in %s", DF_GITCONFIG, HOME_GITCONFIG)
+    lines = ["[include]", f"\tpath = {DF_GITCONFIG}"]
     fsi.append_to_file(HOME_GITCONFIG, lines)
 
 
 def setup_tmux(fsi: FSInterface) -> None:
     """Setup .tmux.conf, the config file for Tmux."""
     if HOME_TMUX_CONF.is_file():
-        logging.warning('%s exists. Not linking %s.', HOME_TMUX_CONF, TMUX_CONF)
+        logging.warning("%s exists. Not linking %s.", HOME_TMUX_CONF, TMUX_CONF)
         return
     fsi.create_link(DF_TMUX_CONF, HOME_TMUX_CONF)
 
@@ -156,19 +156,18 @@ def setup_tmux(fsi: FSInterface) -> None:
 def setup_vimrc(fsi: FSInterface) -> None:
     """Setup .vimrc, the config file for vi/vim."""
     if HOME_VIMRC.is_file():
-        logging.warning(
-            '%s exists. Appending "source %s".', HOME_VIMRC, DF_VIMRC)
-        fsi.append_to_file(HOME_VIMRC, [f'source {DF_VIMRC}'])
+        logging.warning('%s exists. Appending "source %s".', HOME_VIMRC, DF_VIMRC)
+        fsi.append_to_file(HOME_VIMRC, [f"source {DF_VIMRC}"])
     else:
         fsi.create_link(DF_VIMRC, HOME_VIMRC)
+
 
 def setup_initvim(fsi: FSInterface) -> None:
     """Setup init.vim, the config file for Neovim."""
     fsi.ensure_dir_exists(HOME_INITVIM.parent)
     if HOME_INITVIM.is_file():
-        logging.warning(
-            '%s exists. Appending "source %s".', HOME_INITVIM, DF_INITVIM)
-        fsi.append_to_file(HOME_INITVIM, [f'source {DF_INITVIM}'])
+        logging.warning('%s exists. Appending "source %s".', HOME_INITVIM, DF_INITVIM)
+        fsi.append_to_file(HOME_INITVIM, [f"source {DF_INITVIM}"])
     else:
         fsi.create_link(DF_INITVIM, HOME_INITVIM)
 
@@ -186,5 +185,5 @@ def main(argv: list[str]) -> None:
     setup_zshrc(fsi)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv[1:])
